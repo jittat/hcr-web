e94393af227e150c5f44e26c540157c750630921
Jittat Fakcharoenphol <jittat@gmail.com>
2014-10-07 15:41:46 +0700

    added max-walk-distance as options

--- worker.py
+++ worker.py
@@ -48,6 +48,10 @@ def create_network_file(net_filename, options):
                     cmd.append('adv_factor:%f' % rr_rate)
 
             print >> f, ' '.join(cmd)
+    if 'max-walk-distance' in options:
+        max_walk_distance = float(options['max-walk-distance'][0].strip())
+        if max_walk_distance > 0:
+            print >> f, 'max-walk-distance', max_walk_distance
     f.close()
 
 def work(job):
